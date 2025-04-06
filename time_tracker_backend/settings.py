52eacc4f47f7af2ed4ec0abde61123c79afe3496
afmejia <afmejia23@utp.edu.co>
2025-04-06 09:17:14 -0500

    Connecting with postgreSQL

--- time_tracker_backend/settings.py
+++ time_tracker_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "time_tracker_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "timetracker",
+        "USER": "felipe",
+        "PASSWORD": "felipe",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
